d1f60093f330411bb09f91371b7da697abfb64b2
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-03-02 16:18:50 +0000

    Create webpack template project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # todos
+
+Image credits 
+
+Image: Dan Dan / Delete 146 Vectors SVG / PD License.
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./styles.css";
+import deleteIcon from "./images/delete.svg";
+
+const image = document.createElement("img");
+image.src = deleteIcon;
+
+document.body.appendChild(image);
